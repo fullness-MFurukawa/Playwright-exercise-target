60a56292f65be493df4e1d7ea7355a56b0073876
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-05-04 09:31:37 +0900

    セッションの有効性確認機能の実装

--- src/components/providers/NextAuthProvider.tsx
+++ src/components/providers/NextAuthProvider.tsx
@@ -5,7 +5,11 @@ import { SessionProvider } from "next-auth/react";
  * NextAuthのSessionProviderでアプリ全体をラップするプロバイダコンポーネント
  */
 export const NextAuthProvider = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider basePath="/front/api/auth">
-  {children}
-</SessionProvider>;
+  return (<SessionProvider 
+          basePath="/front/api/auth"
+          refetchInterval={60} // 60秒ごとにセッションの状態を確認する
+          refetchOnWindowFocus={true} // ウィンドウにフォーカスが戻った時(タブの切り替えなど)でも確認する
+          >
+      {children}
+     </SessionProvider>);
 };
